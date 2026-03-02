c6379d24ebb783309453f6c0c48844ae0832f601
Lilian Cleret <lilian.cleret@gmail.com>
2026-03-03 00:29:48 +0100

    feat(ui): add custom app title and favicon

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 from app.db.session import engine
 from app.db.base import Base
 from app.models.user import User  # noqa - nécessaire pour que SQLAlchemy connaisse le modèle
@@ -9,7 +11,13 @@ from app.routers import auth, secrets
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Password Manager API")
+app = FastAPI(
+    title="PM API | C-Lilian",
+    description="API sécurisée pour gérer vos secrets",
+    version="1.0.0",
+    docs_url=None,
+    redoc_url=None,
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
@@ -17,6 +25,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# ROUTES
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="PM API | C-Lilian",
+        swagger_favicon_url="/static/pm_logo_nobg.png",
+    )
+
 app.include_router(auth.router)
 app.include_router(secrets.router)
 
